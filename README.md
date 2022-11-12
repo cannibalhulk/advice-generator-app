c36f90a067fe7111004763dc5eb4ba323487d3c1
Cannibalhulk <82077880+Cannibalhulk@users.noreply.github.com>
2022-11-12 15:35:52 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Frontend Mentor - Advice generator app
+
+![Design preview for the Advice generator app coding challenge](./design/desktop-design.jpg)
+
+## Welcome! 👋
+
+Thanks for checking out this front-end coding challenge.
+
+[Frontend Mentor](https://www.frontendmentor.io) challenges help you improve your coding skills by building realistic projects.
+
+**To do this challenge, you need a basic understanding of HTML, CSS and JavaScript.**
+
+## The challenge
+
+Your challenge is to build out this advice generator app using the [Advice Slip API](https://api.adviceslip.com) and get it looking as close to the design as possible.
+
+You can use any tools you like to help you complete the challenge. So if you've got something you'd like to practice, feel free to give it a go.
+
+Your users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Generate a new piece of advice by clicking the dice icon
---
+++ script.js
@@ -0,0 +1,42 @@
+
+//counter elave ettim
+let slip_id=34;
+
+/*funksiya default olaraq 34 deyerini qebul edir
+**her click etdikde counter 1 vahid artir
+**ve novbeti defe click eventi ise dusdukde
+**1 vahid artdigi qiymetden basliyir
+**ve bu usulla her yasil butona vurdugumuzda 
+**bize yeni cumle gosterir
+**/
+const showQuote = (slip_id=34) => {
+    slip_id++
+  fetch(`https://api.adviceslip.com/advice/${slip_id}`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data.slip);
+      const main = document.querySelector("main");
+
+      main.innerHTML = `
+      <div class="card">
+      <p>Advice #${data.slip.id}</p>
+      <h1>"${data.slip.advice}"</h1>
+      <img src="images/pattern-divider-desktop.svg" alt="divider">
+    </div>
+    <div class="dice-btn">
+      <img src="images/icon-dice.svg" alt="dice">
+    </div>`;
+      const diceBtn = document.querySelector(".dice-btn");
+
+      diceBtn.addEventListener("click", () => {
+        showQuote(slip_id);
+      });
+
+    });
+};
+
+const diceBtn = document.querySelector(".dice-btn");
+
+diceBtn.addEventListener("click", () => {
+  showQuote(slip_id);
+});
